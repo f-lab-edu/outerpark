538b43a55d3efe5cf9527274d1262e70be991ec2
sadowbass <shinwa46@naver.com>
2023-11-07 00:25:00 +0900

    init

---
+++ src/main/java/com/sadowbass/outerpark/OuterparkApplication.java
@@ -0,0 +1,13 @@
+package com.sadowbass.outerpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuterparkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OuterparkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sadowbass/outerpark/OuterparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sadowbass.outerpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OuterparkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
